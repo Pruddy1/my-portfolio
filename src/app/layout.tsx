f8a9b6a15317c24209846d4da0b9b8eddd70fd56
Pruddy1 <oreoluwadada6@gmail.com>
2026-09-14 05:38:40 -0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
   title: "Mary Dada — Full-Stack & Security Engineer",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <Footer />
     </html>
   );
 }
